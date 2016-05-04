e1a08d91637ac2c0e0c462c7800833db5259b9c9
Andrew Throener <athroener@gmail.com>
2016-05-04 00:35:52 -0500

    feat(build): working with module builder package

--- dist/client/application.js
+++ dist/client/application.js
@@ -3,6 +3,7 @@
 
   app.registerModule('core', [app.name]);
   app.registerModule('core.config', [app.name]);
+  app.registerModule('core.templates', [app.name]);
   app.registerModule('core.routes', [app.name, 'ui.router']);
 
 })(window.modernMeanApplication);
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,259 +1,70 @@
 'use strict';
 
 import gulp from 'gulp';
-import babel from 'gulp-babel';
-import concat from 'gulp-concat';
-import debug from 'gulp-debug';
-import eslint from 'gulp-eslint';
-import filter from 'gulp-filter';
-import rename from 'gulp-rename';
-import coveralls from 'gulp-coveralls';
-import jeditor from 'gulp-json-editor';
-import ngConfig from 'gulp-ng-config';
-import del from 'del';
-import mainBowerFiles from 'main-bower-files';
-import templateCache from 'gulp-angular-templatecache';
-import imagemin from 'gulp-imagemin';
-import pngquant from 'imagemin-pngquant';
-import { Server as KarmaServer } from 'karma';
-import istanbul from 'gulp-istanbul';
-import mocha from 'gulp-mocha';
-import { clientConfig } from './server/config/config';
-
-var isparta = require('isparta');
-
-function cleanClient() {
-  return del([
-    './dist/client'
-  ]);
-}
-cleanClient.displayName = 'clean:client';
-gulp.task(cleanClient);
-
-function cleanServer() {
-  return del([
-    './dist/server'
-  ]);
-}
-cleanServer.displayName = 'clean:server';
-gulp.task(cleanServer);
-
-function cleanCoverage() {
-  return del([
-    './tests/.coverage'
-  ]);
-}
-cleanCoverage.displayName = 'clean:coverage';
-gulp.task(cleanCoverage);
-
-let clean = gulp.parallel(cleanClient, cleanServer, cleanCoverage);
-clean.displayName = 'clean';
-gulp.task(clean);
-
-function lint() {
-  return gulp.src(['./server/**/*.js', './client/**/*.js', './test/**/*.js', '!**/client/**/*.constants.js', '!**/client/**/*.values.js'])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-}
-lint.displayName = 'lint';
-gulp.task(lint);
-
-function serverBabel() {
-  let filterJS = filter(['**/*.js'], { restore: true });
-  return gulp.src(['./server/**/*.{js,html}'])
-    .pipe(filterJS)
-    .pipe(babel())
-    .pipe(filterJS.restore)
-    .pipe(gulp.dest('./dist/server'));
-}
-serverBabel.displayName = 'babel';
-gulp.task(serverBabel);
-
-function vendor() {
-  let bowerFiles = mainBowerFiles();
-  let filterCSS = filter(['**/*.css'], { restore: true });
-  let filterJS = filter(['**/*.js', '!**/angular.js'], { restore: true });
-  return gulp.src(bowerFiles)
-    .pipe(filterCSS)
-    .pipe(concat('vendor.css'))
-    .pipe(gulp.dest('./dist/client'))
-    .pipe(filterCSS.restore)
-    .pipe(filterJS)
-    .pipe(concat('vendor.js'))
-    .pipe(gulp.dest('./dist/client'));
-}
-vendor.displayName = 'vendor';
-gulp.task(vendor);
-
-function templates() {
-  return gulp.src(['./client/**/*.html'])
-    .pipe(templateCache({
-      root: 'modern-mean-core-material/',
-      module: 'core'
-    }))
-    .pipe(gulp.dest('./dist/client'));
-}
-templates.displayName = 'templates';
-gulp.task(templates);
-
-function bootloader() {
-  return gulp.src(['./client/app/core.client.app.loader.js'])
-          .pipe(rename('bootloader.js'))
-          .pipe(gulp.dest('./dist/client'));
-}
-bootloader.displayName = 'bootloader';
-gulp.task(bootloader);
-
-function angular() {
-  let bowerFiles = mainBowerFiles();
-  let angularJS = filter(['**/angular.js'], { restore: false });
-  return gulp.src(bowerFiles)
-    .pipe(angularJS)
-    .pipe(rename('angular.js'))
-    .pipe(gulp.dest('./dist/client'));
-}
-angular.displayName = 'angular';
-gulp.task(angular);
-
-function application() {
-  let filterJS = filter(['**/*.js'], { restore: true }),
-    filterCSS = filter(['**/*.css'], { restore: true });
-
-  return gulp.src(['./client/**/*.module.js', './client/**/*.{js,css}', '!**/core.client.app.loader.js'])
-    .pipe(filterJS)
-    .pipe(concat('application.js'))
-    .pipe(gulp.dest('./dist/client'))
-    .pipe(filterJS.restore)
-    .pipe(filterCSS)
-    .pipe(concat('application.css'))
-    .pipe(gulp.dest('./dist/client'));
-}
-application.displayName = 'application';
-gulp.task(application);
-
-function images() {
-  return gulp.src(['./client/**/*.{jpg,png,gif,ico}'])
-  .pipe(imagemin({
-        progressive: true,
-        svgoPlugins: [
-          {removeViewBox: false},
-          {cleanupIDs: false}
-        ],
-        use: [pngquant()]
-      }))
-    .pipe(gulp.dest('./dist/client'));
-}
-images.displayName = 'images';
-gulp.task(images);
+import * as modules from 'modern-mean-build-gulp/dist/modules';
+import { clientConfig as config } from './server/config/config';
 
 
-function testClientSingle(done) {
+
+function setTest(done) {
   process.env.NODE_ENV = 'test';
-  new KarmaServer({
-    configFile: process.cwd() + '/tests/karma.conf.js',
-    singleRun: true
-  }, done).start();
+  process.env.MM_MONGOOSE_DB = 'modern-mean-test';
+  return done();
 }
-testClientSingle.displayName = 'test:client';
-gulp.task(testClientSingle);
 
-function testServerSingle(done) {
-  gulp.src(['./server/**/*.js'])
-  	.pipe(istanbul({
-      instrumenter: isparta.Instrumenter,
-      includeUntested: true
-    }))
-  	.pipe(istanbul.hookRequire()) // or you could use .pipe(injectModules())
-  	.on('finish', function () {
-  	  gulp.src(['./tests/server/**/*.js'])
-      //.pipe(injectModules())
-  		.pipe(mocha({
-        reporter: 'spec',
-        require: ['./tests/mocha.setup'],
-      }))
-  		.pipe(istanbul.writeReports(
-        {
-          dir: './tests/.coverage/server',
-          reporters: [ 'lcov', 'html', 'text' ]
-        }
-      ))
-      .once('error', () => {
-        process.exit(1);
-        return done();
-      })
-      .once('end', () => {
-        return done();
-      });
-  	});
+function clientConfig(done) {
+  process.env.MM_MODULE_CLIENT_CONSTANTS = JSON.stringify(config.constants);
+  process.env.MM_MODULE_CLIENT_VALUES = JSON.stringify(config.values);
+  process.env.MM_MODULE_ROOT = 'modern-mean-core-material';
+  process.env.MM_MODULE_ANGULAR = 'core';
+  return done();
 }
-testServerSingle.displayName = 'test:server';
-gulp.task(testServerSingle);
-
-function ssl() {
-  return gulp.src('server/ssl/*')
-    .pipe(gulp.dest('./dist/server/ssl'));
-}
-ssl.displayName = 'ssl';
-gulp.task(ssl);
-
-function buildConfig() {
-  return gulp.src('./server/config/client.json')
-    .pipe(jeditor(function(json) {
-      let stringy = JSON.stringify(clientConfig.constants); // must return JSON object.
-      return JSON.parse(stringy);
-    }))
-    .pipe(ngConfig('core.config', {
-      wrap: true,
-      createModule: false
-    }))
-    .pipe(rename('core.client.config.constants.js'))
-    .pipe(gulp.dest('./client/config'))
-    .pipe(gulp.src('./server/config/client.json'))
-    .pipe(jeditor(function(json) {
-      let stringy = JSON.stringify(clientConfig.values); // must return JSON object.
-      return JSON.parse(stringy);
-    }))
-    .pipe(ngConfig('core.config', {
-      type: 'value',
-      wrap: true,
-      createModule: false
-    }))
-    .pipe(rename('core.client.config.values.js'))
-    .pipe(gulp.dest('./client/config'));
-
-}
-buildConfig.displayName = 'client:config';
-gulp.task(buildConfig);
 
 
-
-function sendCoveralls() {
-  return gulp.src('tests/.coverage/**/lcov.info')
-    .pipe(debug())
-    .pipe(concat('lcov.info'))
-    .pipe(coveralls());
-}
-sendCoveralls.displayName = 'coveralls';
-gulp.task(sendCoveralls);
 
 
 //Build Client
-let client = gulp.series(cleanClient, buildConfig, gulp.parallel(images, templates, application, vendor, bootloader, angular));
+let client = gulp.series(modules.client.clean, clientConfig, modules.client.build, modules.client.angular, modules.client.bootloader);
 client.displayName = 'client';
 gulp.task(client);
 
 //Build Server
-let server = gulp.series(cleanServer, buildConfig, gulp.parallel(serverBabel, ssl));
+let server = gulp.series(modules.server.clean, gulp.parallel(modules.server.application));
 server.displayName = 'server';
 gulp.task(server);
 
 //Gulp Default
-let defaultTask = gulp.series(clean, buildConfig, gulp.parallel(server, client));
+//let defaultTask = gulp.series(modules.clean, modules.server.config, gulp.parallel(modules.client.build, modules.server.build));
+let defaultTask = gulp.parallel(client, server);
 defaultTask.displayName = 'default';
 gulp.task(defaultTask);
 
+//Gulp Default
+let lint = gulp.series(modules.lint.all);
+lint.displayName = 'lint';
+gulp.task(lint);
+
 //Gulp Test
-let testTask = gulp.series(defaultTask, lint, testClientSingle, testServerSingle);
+let testTask = gulp.series(modules.lint.all, defaultTask, modules.test.client, modules.test.server);
 testTask.displayName = 'test';
 gulp.task(testTask);
+
+//Gulp TestClient
+let testClient = gulp.series(modules.lint.client, defaultTask, modules.test.client);
+testClient.displayName = 'test:client';
+gulp.task(testClient);
+
+//Gulp TestServer
+let testServer = gulp.series(modules.lint.server, defaultTask, modules.test.server);
+testServer.displayName = 'test:server';
+gulp.task(testServer);
+
+//Gulp Coverage
+let coverage = gulp.series(modules.test.coverage);
+coverage.displayName = 'coverage';
+gulp.task(coverage);
+
+//Gulp Clean
+let clean = gulp.series(modules.clean);
+clean.displayName = 'clean';
+gulp.task(clean);
